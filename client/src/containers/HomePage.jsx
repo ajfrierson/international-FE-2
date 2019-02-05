3ce7a84d5f33ab58b739f9b1ed4fa3b4b5a125dc
joepound <14357797+joepound@users.noreply.github.com>
2019-02-05 16:20:24 -0700

    Can now retrieve child info (using requests to temporary server)

--- client/src/containers/HomePage.jsx
+++ client/src/containers/HomePage.jsx
@@ -2,26 +2,62 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { logout } from '../store/actions';
+import { getChildren, logout } from '../store/actions';
 
-const HomePage = props => {
-  return (
-    <>
-      <div>Home</div>
-      <button type='button' onClick={props.logout}>
-        Log Out
-      </button>
-    </>
-  );
-};
+import './HomePageStyles.css';
 
-HomePage.propTypes = {
-  logout: PropTypes.func.isRequired
+class HomePage extends React.Component {
+  static propTypes = {
+    children: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        status: PropTypes.string.isRequired,
+        age: PropTypes.number.isRequired,
+        insuranceCard: PropTypes.string,
+        birthCertificateExpires: PropTypes.string,
+        specialNeeds: PropTypes.string,
+        representative: PropTypes.string,
+        contactInfo: PropTypes.string
+      })
+    ).isRequired,
+    getChildren: PropTypes.func.isRequired,
+    logout: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.props.getChildren();
+  }
+
+  render() {
+    return (
+      <>
+        <div>Home</div>
+        <button type='button' onClick={this.props.logout}>
+          Log Out
+        </button>
+        <div className='childrenGrid'>
+          {this.props.children.map(child => (
+            <div className='childrenGrid__child' key={child.id}>
+              <h3>{child.name}</h3>
+              <div>Status: {child.status}</div>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    children: state.childReducer.children
+  };
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   {
+    getChildren,
     logout
   }
 )(HomePage);
