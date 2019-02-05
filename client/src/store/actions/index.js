f13e8a441525cecd2876b3a0f609ee7618780533
joepound <14357797+joepound@users.noreply.github.com>
2019-02-05 16:20:24 -0700

    Can now retrieve student info (using requests to temporary server)

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,18 +1,28 @@
 import axios from 'axios';
 
 import {
+  // General actions
   HANDLE_TEXT_INPUT_CHANGE,
+  // loginReducer actions
   LOGIN_START,
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
-  LOGOUT
+  LOGOUT,
+  // childReducer actions
+  FETCH_CHILDREN_START,
+  FETCH_CHILDREN_SUCCESS,
+  FETCH_CHILDREN_FAILURE
 } from './types';
 
 const baseURL = 'http://localhost:5000';
 
+// General action definitions
+
 export const handleTextInputChange = e => dispatch => {
   dispatch({ type: HANDLE_TEXT_INPUT_CHANGE, payload: e.currentTarget });
 };
+
+// loginReducer action definitions
 
 export const login = (loginUsername, loginPassword) => dispatch => {
   dispatch({ type: LOGIN_START });
@@ -26,3 +36,13 @@ export const login = (loginUsername, loginPassword) => dispatch => {
 };
 
 export const logout = () => dispatch => dispatch({ type: LOGOUT });
+
+// childReducer action definitions
+
+export const getChildren = () => dispatch => {
+  dispatch({ type: FETCH_CHILDREN_START });
+  axios
+    .get(`${baseURL}/api/students`)
+    .then(res => dispatch({ type: FETCH_CHILDREN_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: FETCH_CHILDREN_FAILURE, payload: err }));
+};
--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,7 +1,9 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-import loginReducer from "./loginReducer";
+import loginReducer from './loginReducer';
+import childReducer from './childReducer';
 
 export default combineReducers({
-  loginReducer
+  loginReducer,
+  childReducer
 });
