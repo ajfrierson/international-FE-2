0add65a1bdd5bf087c1a30e9b62cd5a0b9bf4ed9
joepound <14357797+joepound@users.noreply.github.com>
2019-02-06 11:59:05 -0700

    Fixed issue with browsers crashing if Redux Dev Tools is not installed

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,10 +3,13 @@ import rootReducer from "./reducers/index";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
+const middleware = [logger, thunk];
+const reduxDevToolsEnabler = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 export default createStore(
   rootReducer,
-  compose(
-    applyMiddleware(logger, thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  reduxDevToolsEnabler ? compose(
+    applyMiddleware(...middleware),
+    reduxDevToolsEnabler
+  ) : applyMiddleware(...middleware)
 );
